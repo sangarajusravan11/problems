1bc1e9e337d3f1003af299ae2146591596ed5247
Student college <student@collage>
2025-04-23 22:24:49 +0530

    commit

---
+++ src/problems/CharArrayToString.java
@@ -0,0 +1,13 @@
+package problems;
+
+public class CharArrayToString {
+		public static void main(String[]arga)
+		{
+			char[] charArray= {'s','r','a','v','a','n'};
+			String res=new String(charArray);
+			System.out.println(res); 
+			
+		}
+}
+
+
